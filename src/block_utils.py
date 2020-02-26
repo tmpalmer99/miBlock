efc4a6c94c61fb4ea1fba6a3413dcbeee5e598ca
Tom Palmer <sc17tp@leeds.ac.uk>
2020-02-26 12:28:57 +0000

    Fixing repo problem

---
+++ src/block_utils.py
@@ -0,0 +1,26 @@
+from src.block import Block
+
+
+def get_block_object_from_dict(block_dict):
+    """
+    Blocks are sometimes represented in their dictionary form to allow JSON serialisation
+    of block. 'get_object_from_dict' returns a block to a Block object.
+
+    :param block_dict:  A dictionary representing a block object
+    :return:            A Block object
+    """
+    index = block_dict["index"]
+    previous_hash = block_dict["previous_hash"]
+    timestamp = block_dict["timestamp"]
+    nonce = block_dict["nonce"]
+    # Todo Records may need to be converted from dict representation
+    records = block_dict["records"]
+    true_hash = block_dict["hash"]
+
+    block = Block(index, previous_hash, timestamp, records, nonce)
+    block_hash = block.get_block_hash()
+    block.hash = block_hash
+
+    # Ensure block data is correct
+    if block_hash == true_hash:
+        return block
---
+++ src/maintenance_record.py
@@ -0,0 +1,34 @@
+import hashlib
+import os
+
+
+class MaintenanceRecord:
+    aircraft_reg = None
+    date_of_record = None
+    record_filename = None
+    record_hash = None
+
+    def __init__(self, aircraft_registration_number, date_of_record, filename):
+        self.aircraft_reg = aircraft_registration_number
+        self.date_of_record = date_of_record
+        self.record_filename = filename
+        self.record_hash = self.get_file_hash()
+
+    def get_file_hash(self):
+        file_hash = hashlib.sha256()
+        reading_file = True
+
+        try:
+            file = open(f"{os.getcwd()}/{self.record_filename}", "rb")
+        except (OSError, IOError) as e:
+            print(str(e))
+            return
+
+        with file:
+            while reading_file:
+                data = file.read(4096)
+                if data == "":
+                    reading_file = False
+                else:
+                    file_hash.update(data)
+        return file_hash
